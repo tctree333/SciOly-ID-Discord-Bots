3a90b6416c8443a6cc5e0822fd73e86699fe4fa7
Tomi Chen <tctree333@users.noreply.github.com>
2020-02-22 17:52:25 -0800

    update script

--- scripts/generate_file_structure.py
+++ scripts/generate_file_structure.py
@@ -1,13 +1,14 @@
 import os
 
+PATH="PATH_TO/Fossil-ID/data/lists"
 def _groups():
     """Converts txt files of data into lists."""
-    filenames = [name.strip(".txt") for name in os.listdir("PATH_TO/Fossil-ID/bot/data/lists")]
+    filenames = [name.strip(".txt") for name in os.listdir(PATH)]
     # Converts txt file of data into lists
     lists = {}
     for filename in filenames:
         print(f"Working on {filename}")
-        with open(f'PATH_TO/Fossil-ID/bot/data/lists/{filename}.txt', 'r') as f:
+        with open(f'{PATH}/{filename}.txt', 'r') as f:
             lists[filename] = [line.strip().lower() for line in f]
         print(f"Done with {filename}")
     print("Done with lists!")
@@ -17,7 +18,11 @@ groups = _groups()
 
 for key in groups.keys():
     for item in groups[key]:
-        os.makedirs(f"{key}/{item}")
-        with open(f"{key}/{item}/image.placeholder", "x") as f:
-            f.write("")
+        try:
+            os.makedirs(f"{key}/{item}")
+        except OSError:
+            print("Already exists")
+        else:
+            with open(f"{key}/{item}/image.placeholder", "x") as f:
+                f.write("")
 
